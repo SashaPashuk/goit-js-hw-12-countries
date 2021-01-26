b522f6311bbe8868d890e90b920785bd323db9df
Alexander <pashuk.sasha18@gmail.com>
2021-01-26 21:22:49 +0200

    initial commit

--- src/js/pnotify.js
+++ src/js/pnotify.js
@@ -5,3 +5,16 @@ import '@pnotify/core/dist/BrightTheme.css'
 const myPnotify = (title) => error({ title, delay: 2000 }, 200 )
 
 const tooManyCountries = () => myPnotify('Too Many Matches Found')
+
+const notFound = () => myPhotify('Not Found')
+
+const somethingWentWrong = () => myPhotify('Something went wrong')
+
+const errorMessage = (status) => {
+    status === 404 ? notFound(): somethingWentWrong()
+}
+
+export default {
+    tooManyCountries,
+    error: errorMessage,
+}
