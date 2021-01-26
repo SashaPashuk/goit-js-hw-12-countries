a3e8681d1b02d424202df3f773a4e3b3c9f53fda
Alexander <pashuk.sasha18@gmail.com>
2021-01-26 20:57:38 +0200

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,26 @@
+import debounce from 'lodash.debounce'
+
+import { refs } from './constans'
+import API from './services'
+import { coutriesListMarkup } from './markups'
+import { render } from 'node-sass'
+
+const handleChangeInput = async ({ target: { value } }) => {
+    if (value) {
+        const response = await API.fetchCountries(value)
+        response.length > 0 && reander(response)
+        return
+    }
+    !value && clearResultContainer()
+}
+
+const handleDebounceTyping = debounce(handleChangeInput, 500)
+$input.addEventListener('input', handleDebounceTyping)
+
+function reander(items) {
+    $resultContainer.innerHTML = countriesListMarkup(items)
+}
+
+function clearResultContainer() {
+    $resultContainer.innerHTML = ''
+}
